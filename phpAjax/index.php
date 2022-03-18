38497b38b57b897b5745b6ed9b0c16809e0dfa40
thomas-ayissi <thomas8sea@gmail.com>
2022-03-18 16:07:02 +0100

    Ajout de "acces control" dans le header

--- phpAjax/index.php
+++ phpAjax/index.php
@@ -1,6 +1,17 @@
 <?php
 
+//TODO 2 - ven 18 Mars 2022  - Ajouter un fichier "config" pour avoir des informations de connexion à la BDD
+
+$dbh = new PDO('mysql:host=localhost;dbname= js_bdd_exo_ajax_jeu17mars2022', $user, $pass);
+
+
 echo '{"nom":"Smith","prenom":"John","age":42,"pays":"Ukraine"}'
+
+
+
+
+
+
 ?>
 
 
